5ca99553fc58d7996d1da55fc1cc38d8da122549
Alex <fedoseev_av91@mail.ru>
2023-07-26 18:25:03 +0300

    add void GetSecondArray

--- Program.cs
+++ Program.cs
@@ -1,5 +1,19 @@
 ﻿
 
+
+void GetSecondArray(string[] firstArray, string[] secondArray)
+{
+    int count = 0;
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+        if (firstArray[i].Length <= 3)
+        {
+            secondArray[count] = firstArray[i];
+        }
+        count++;
+    }
+}
+
 void PrintArray(string[] inArray)
 {
     for (int i = 0; i < inArray.GetLength(0); i++)
